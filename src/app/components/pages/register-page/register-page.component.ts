d398de604d563eaed651d0bc9e2910bee6187cca
TarkaniiArtur <arturmandragon@gmail.com>
2023-04-05 19:35:12 +0300

    GOIT-5, added register page

--- src/app/components/pages/register-page/register-page.component.ts
+++ src/app/components/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { FormsService } from 'src/app/services/forms.service';
 
 @Component({
   selector: 'app-register-page',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup;
+  public passwordMinLength: number = this.formsService.passwordMinLength;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private formsService: FormsService
+  ) { 
+    this.registerForm = this.formBuilder.group({
+      email: ['', this.formsService.emailValidator],
+      password: ['', this.formsService.passwordValidator],
+      repeatPassword: ['', this.repeatPasswordValidator],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  // Getters
+
+  public get emailControl(): AbstractControl | null {
+    return this.registerForm.get('email');
+  }
+
+  public get passwordControl(): AbstractControl | null {
+    return this.registerForm.get('password');
+  }
+
+  public get repeatPasswordControl(): AbstractControl | null {
+    return this.registerForm.get('repeatPassword');
+  }
+
+  // Validators
+  
+  private get repeatPasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control?.value) return { required: true };
+
+      if (this.passwordControl?.valid && this.passwordControl?.value !== control.value) return { mismatch: true }; 
+
+      return null;
+    }
+  }
+
+  // Register
+
+  public register(): void {
+    console.log('form:', this.registerForm?.value);
+    this.registerForm.markAllAsTouched();
+    for ( const control in this.registerForm.controls ) {
+      this.registerForm.get(control)?.updateValueAndValidity();
+    }
   }
 
 }
