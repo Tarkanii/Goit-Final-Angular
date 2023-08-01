5549dfb4b64e60313e610d8b02878a2ab5ab1d44
TarkaniiArtur <arturmandragon@gmail.com>
2023-08-01 22:56:15 +0300

    GOIT-26, added password visibility switcher

---
+++ src/app/shared/directives/visibility-switcher.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VisibilitySwitcherDirective } from './visibility-switcher.directive';
+
+describe('VisibilitySwitcherDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VisibilitySwitcherDirective();
+    expect(directive).toBeTruthy();
+  });
+});
