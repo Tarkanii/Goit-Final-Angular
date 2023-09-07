a89e9c7b05822bbd63dc9cbda394a114c406c645
TarkaniiArtur <arturmandragon@gmail.com>
2023-09-07 19:31:36 +0300

    GOIT-40, fixed participants list

--- src/app/components/forms/add-people/add-people.component.ts
+++ src/app/components/forms/add-people/add-people.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, filter, take } from 'rxjs';
+import { Observable, Subject, combineLatest, filter, map, take, takeUntil } from 'rxjs';
 import { FormsService } from 'src/app/services/forms.service';
 import { IProject } from 'src/app/shared/interfaces/project';
 import { IStore } from 'src/app/shared/interfaces/store';
 import { addParticipantAction, setSidebarFormAction } from 'src/app/store/projects/projects.actions';
 import { projectSelector } from 'src/app/store/projects/projects.selectors';
+import { emailSelector } from 'src/app/store/user/user.selectors';
 
 @Component({
   selector: 'app-add-people',
   templateUrl: './add-people.component.html',
   styleUrls: ['./add-people.component.scss']
 })
-export class AddPeopleComponent implements OnInit {
+export class AddPeopleComponent implements OnInit, OnDestroy {
 
   public addPeopleForm: FormGroup | null = null;
   public project$!: Observable<IProject | undefined>;
+  public participantsList$!: Observable<String[]>;
   public projectId: string = '';
+  private unsubscibe$: Subject<void> = new Subject();
 
   constructor(
     private store: Store<IStore>,
@@ -41,6 +44,19 @@ export class AddPeopleComponent implements OnInit {
     
     this.projectId = this.router.url.split('/')[2];
     this.project$ = this.store.select(projectSelector(this.projectId));
+    this.participantsList$ = combineLatest([this.project$, this.store.select(emailSelector)]).pipe(
+      map(([project, email]) => {
+        if (!project) return [];
+
+        return project.participants.filter((participant: string) => participant !== email);
+      }),
+      takeUntil(this.unsubscibe$)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscibe$.next();
+    this.unsubscibe$.complete();
   }
 
   // Getters
